5f268f8dc772243a0efbfacb060c9baa48c5c27d
LTLeonid <contact@ltleonid.ru>
2026-08-27 19:23:58 +0000

    go part ended, start c#

--- gopart/main.go
+++ gopart/main.go
@@ -1,22 +1,62 @@
-package main //Надо всегда начинать с пакагов
+package main
+
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"math"
+	"io"
+	"net/http"
 )
 
-func main() {
-	// height := 1.8
-	// var weight float64
-	// weight = 100
-	// height, weight := 1.8 , 100 // Динам.-стат. распоковка
-	const imtpower float64 = 2
-	var height, weight float64 = 1.8, 100 // множественная распоковка
-	fmt.Print("Enter Height:")
-	fmt.Scan(&height) // Переменные передаётся по ссылке -> иначе была бы копия, привет с++
-	fmt.Print("Enter Weight: ")
-	fmt.Scan(&weight)
-	var imt = weight / math.Pow(height, imtpower)
-	fmt.Print(imt)
+// const SharpURL = "http://127.0.0.1:8081/process"
+const SharpURL = "https://httpbin.org/post"
+
+type DataText struct {
+	Text    *string  `json:"text"`
+	Provide []string `json:"provide"` //фига забавно массивы
 }
 
-// var imt = float64(weight) / weight // Эт явные преобразы
+func gateway(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		ctx_byte, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Printf("Error %v", err)
+			return
+		}
+		ctx := string(ctx_byte)
+		fmt.Print(ctx)
+		var data DataText
+		data.Text = &ctx
+		data.Provide = append(data.Provide, "go") //лять, серъёзно?? реасигн вместо n+1, чзх
+		r.Body.Close()
+		jsonpayload, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		req, err := http.NewRequest(
+			http.MethodPost,
+			SharpURL,
+			bytes.NewBuffer(jsonpayload),
+		)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+		res, err := http.DefaultClient.Do(req)
+		res.Body.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}
+}
+
+func main() {
+	print("handle /")
+	http.HandleFunc("/", gateway)
+	print("start serve")
+	http.ListenAndServe(":8080", nil)
+}
