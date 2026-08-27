4a5c5e69ef69b6298c87babff9903c25644327e5
LTLeonid <contact@ltleonid.ru>
2026-08-27 17:47:56 +0000

    var usage x3, io usage

--- gopart/main.go
+++ gopart/main.go
@@ -11,6 +11,10 @@ func main() {
 	// height, weight := 1.8 , 100 // Динам.-стат. распоковка
 	const imtpower float64 = 2
 	var height, weight float64 = 1.8, 100 // множественная распоковка
+	fmt.Print("Enter Height:")
+	fmt.Scan(&height) // Переменные передаётся по ссылке -> иначе была бы копия, привет с++
+	fmt.Print("Enter Weight: ")
+	fmt.Scan(&weight)
 	var imt = weight / math.Pow(height, imtpower)
 	fmt.Print(imt)
 }
